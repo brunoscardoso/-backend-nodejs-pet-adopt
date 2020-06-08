ced135c8d435a359ae5f35d3aab38c7377bd2fca
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2020-06-08 18:06:46 -0300

    add gifted point db

--- src/database/migrations/0_create_adopt.ts
+++ src/database/migrations/0_create_adopt.ts
@@ -13,6 +13,7 @@ export async function up(knex: Knex) {
     table.decimal('latitude').notNullable();
     table.decimal('longitude').notNullable();
     table.boolean('adopted').notNullable().defaultTo(false);
+    table.boolean('gifted').notNullable().defaultTo(false);
   });
 }
 
